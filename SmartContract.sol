8af0bffeeb47d0022009c71649537f2569a286d7
christianbaje <88245380+christianbaje@users.noreply.github.com>
2023-07-23 18:08:46 +0800

    Update SmartContract.sol

--- SmartContract.sol
+++ SmartContract.sol
@@ -4,21 +4,21 @@ pragma solidity ^0.8.0;
 contract SmartContract {
     uint256 public someValue;
 
-    function require(uint256 value) public {
+    function requireE(uint256 value) public {
         // The require statement is used to check conditions that users must fulfill.
         // If the condition is not met, the transaction will be reverted, and any changes will be rolled back.
         require(value > 0, "Value must be greater than 0");
         someValue = value;
     }
 
-    function assert(uint256 value) public {
+    function assertE(uint256 value) public {
         // The assert statement is used to check internal errors and conditions that should never occur.
         // If the condition is not met, the contract execution will be halted, and all state changes will be reverted.
         assert(value != 0);
         someValue = value;
     }
 
-    function revert(uint256 value) public {
+    function revertE(uint256 value) public {
         // The revert statement is used to handle custom error conditions.
         // It is commonly used to provide informative error messages to users.
         if (value == 0) {
